bc4787345aecae2288ee012d5dcd732d98de3c1c
voftec <info@voftec.com>
2025-06-02 02:36:19 -0300

    updated credentials
    updates credentials

--- public/firebase/firebase-config.js
+++ public/firebase/firebase-config.js
@@ -1,38 +1,12 @@
-// public/firebase/firebase-config.js
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from 'firebase/database';
-import { getAuth } from 'firebase/auth';
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAg8Eza9ETRPCRwjnt1rJoBdmqkqXunDAI",
-  authDomain: "voftec.firebaseapp.com",
-  databaseURL: "https://voftec-default-rtdb.firebaseio.com",
-  projectId: "voftec",
-  storageBucket: "voftec.firebasestorage.app",
-  messagingSenderId: "485422164439",
-  appId: "1:485422164439:web:22187c2d498e17022bc392",
-  measurementId: "G-6X4EG9SWP5"
+  apiKey: "AIzaSyByizd4iTk9PxRpCraFTltMEtRYVocuXVY",
+  authDomain: "polkadot-astranet.firebaseapp.com",
+  databaseURL: "https://polkadot-astranet-default-rtdb.firebaseio.com",
+  projectId: "polkadot-astranet",
+  storageBucket: "polkadot-astranet.firebasestorage.app",
+  messagingSenderId: "604267324876",
+  appId: "1:604267324876:web:5f6231275fa5a3b9dde969",
+  measurementId: "G-Y8FSJNGVWG"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const database = getDatabase(app);
-const auth = getAuth(app);
-
-// Export the initialized services
-export {
-  app,
-  database,
-  auth,
-  analytics,
-  firebaseConfig
-};
-
-// Export default configuration
 export default firebaseConfig;
